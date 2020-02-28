204e4b6b8663da4c0eaefab4c31f8de7df5d416d
umesh1987 <umesh.state@gmail.com>
2020-02-27 18:50:46 -0600

    write database query and runn into mysql workbench

---
+++ db/schema.sql
@@ -0,0 +1,48 @@
+-- creating the burgers database
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers(
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(40) NOT NULL,
+    devoured BOOLEAN default false,
+    PRIMARY KEY(id)
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+DROP DATABASE IF EXISTS ice_creamDB;
+
+CREATE DATABASE ice_creamDB;
+
+USE ice_creamDB;
+
+CREATE TABLE products (
+  id INT NOT NULL AUTO_INCREMENT,
+  flavor VARCHAR(45) NULL,
+  price DECIMAL(10,2) NULL,
+  quantity INT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO products (flavor, price, quantity)
+VALUES ("vanilla", 2.50, 100);
+
+INSERT INTO products (flavor, price, quantity)
+VALUES ("chocolate", 3.10, 120);
+
+INSERT INTO products (flavor, price, quantity)
+VALUES ("strawberry", 3.25, 75);
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+-- insert into burger_db
+USE burger_db;
+INSERT INTO burgers(burger_name, devoured) VALUES("Whiskey King Burger", false);
+INSERT INTO burgers(burger_name, devoured) VALUES("The Lola Burger", false);
+INSERT INTO burgers(burger_name, devoured) VALUES("Cheeseburger", false);
+INSERT INTO burgers(burger_name, devoured) VALUES("Buckhorn Burger", false);
+INSERT INTO burgers(burger_name, devoured) VALUES("Primetime Burger", false);
+INSERT INTO burgers(burger_name, devoured) VALUES("Hamburger", false);
