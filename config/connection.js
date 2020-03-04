3c89f3fbd6945e4ab75fc19c5815f80a87e238ee
umesh1987 <umesh.state@gmail.com>
2020-03-04 11:15:00 -0600

    update root name and password

--- config/connection.js
+++ config/connection.js
@@ -7,11 +7,11 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: "s9xpbd61ok2i7drv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        host: "localhost",
         port: 3306,
-        user: "gsccdpy0e0up4l7v",
-        password: "ks0ivb2r6vrvfibh",
-        database: "viwvhtbio2l0x74y"
+        user: "root",
+        password: "",
+        database: "burgers_db"
 
     })
 }
