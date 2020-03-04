b619944a21332016ef803466a4fb26107b983fca
umesh1987 <umesh.state@gmail.com>
2020-03-04 08:50:59 -0600

    add heroku user name and password to connection heroku

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,20 @@
 // connect to MySQL.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "burgers_db"
-});
+var connection;
 
-// Make connection.
-connection.connect(function(err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "s9xpbd61ok2i7drv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        port: 3306,
+        user: "gsccdpy0e0up4l7v",
+        password: "ks0ivb2r6vrvfibh",
+        database: "viwvhtbio2l0x74y"
+
+    })
+}
+connection.connect();
+
 module.exports = connection;
